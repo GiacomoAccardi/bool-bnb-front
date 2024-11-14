8c736067568a60be9831afbfafc1aec607551780
MattiaPiazzolla <mattiapiazzolla@gmail.com>
2024-11-14 19:05:38 +0100

    stilizzato navbar

--- src/components/AppNavbar.vue
+++ src/components/AppNavbar.vue
@@ -2,35 +2,27 @@
 export default {};
 </script>
 <template>
-	<div class="row sticky z-10">
-		<div class="col-2">
-			<!-- <img class="logo" src="../assets/BOOL-BNB-DB.png" alt="Logo"> -->
-			<h1 class="logo-config rainbow3">Bool-b&b</h1>
-		</div>
-		<div class="col-10">
-			<div class="content">
-				<ul class="list-unstyled mt-3 d-flex justify-content-end">
-					<li class="mx-3">
-						<router-link
-							to="/"
-							class="text-decoration-none text-black"
-							active-class="active-link"
-							exact>
-							Home
-						</router-link>
-					</li>
-					<li class="mx-3">
-						<a href="#" class="text-decoration-none text-black">About US</a>
-					</li>
-					<li class="mx-3">
-						<a href="#" class="text-decoration-none text-black">Activities</a>
-					</li>
-					<li class="mx-3">
-						<a href="#" class="text-decoration-none text-black">Contacts</a>
-					</li>
-				</ul>
+	<div class="container">
+		<div class="row navbarcustom sticky">
+			<div class="col-2">
+				<h1 class="logo-config rainbow3">Bool-b&b</h1>
+			</div>
+			<div class="col-10">
+				<div class="content">
+					<ul class="list-unstyled mt-3 d-flex justify-content-end">
+						<li class="mx-3">
+							<a href="#Home" class="text-secondary text-decoration-none"
+								>Affitta con Bool b&b</a
+							>
+						</li>
+					</ul>
+				</div>
 			</div>
 		</div>
 	</div>
 </template>
-<style lang=""></style>
+<style lang="scss">
+.navbarcustom {
+	z-index: 1000;
+}
+</style>
